4245faaf6a341ef5a99f3368dfed968948fcfa8b
AyuSh <101919645+ayuSh0614@users.noreply.github.com>
2024-11-15 01:18:56 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,23 @@
 const app = require('./app');
 const PORT = process.env.PORT || 5000;
 
+const cors = require('cors')
 
+const RegisterModel = require('./models/Register')
+
+const app = express()
+
+app.use(cors(
+    {
+
+        origin: ["https://deploy-mern-1whq.vercel.app"], 
+        methods: ["POST", "GET"],
+        
+        credentials: true
+
+    }
+
+));
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
